65dc7d377d6932350873daffa4dbb689503e1517
hiyoshi <hiyoshi_kyota@r-net.co.jp>
2020-04-02 00:39:07 -0700

    WebController.javaをAPI仕様に変更

--- src/main/java/com/example/validatingforminput/WebController.java
+++ src/main/java/com/example/validatingforminput/WebController.java
@@ -1,49 +1,19 @@
 package com.example.validatingforminput;
 
+import java.util.HashMap;
 import javax.validation.Valid;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("getSyainInfo")
+public class WebController {
 
-@Controller
-public class WebController implements WebMvcConfigurer {
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/results").setViewName("results");
-	}
-	
-	
-	@GetMapping("/")
-	public String showForm(PersonForm personForm) {
-		return "form";
-	}
-	@GetMapping("/page")
-	public String showPage(PersonForm personForm) {
-		return "page";
+	@RequestMapping(method = RequestMethod.GET)
+	public String getSyainInfo() {
+		return "SyainInfo";
 	}
 
-	@PostMapping("/")
-	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
-
-		if (bindingResult.hasErrors()) {
-			return "form";
-		}
-
-		return "redirect:/results";
-	}
-
-	@PostMapping("/page")
-	public String checkPersonInfo(PersonForm personForm) {
-
-		
-
-		return "redirect:/page";
-	}
-	
 }
